b64a164dd038b374d59fb183e3becb2d5ef3b407
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-11-07 23:35:35 -0200

    Desenvolvendo tela login

--- client/src/components/signIn/SignIn.vue
+++ client/src/components/signIn/SignIn.vue
@@ -1,34 +1,37 @@
 <template>
   <div class="hello">
    <div class="wrapp">
-    <h2 class="title"> Login </h2>
+    <!-- <v-app id="inspire"> -->
+        <h2 class="title"> Login </h2>
 
-    <div class="weather-image-wrapper">
-      <img src="../../assets/images/unlocked.svg" alt="Login" class="account-image">
-    </div>
-
-    <form class="wrapp-form" v-model="valid" ref="form" lazy-validation>
-      <v-text-field
-        class="color size"
-        v-model="email"
-        label="E-mail"
-        :rules="emailRules"
-        data-vv-name="email"
-        required>
-      </v-text-field>
-      <v-text-field
-        class="color size"
-        type="password"
-        v-model="pass"
-        label="Senha"
-        data-vv-name="pass"
-        required>
-      </v-text-field>
-
-      <router-link :to="{ name: 'Timeline' }">
-        <v-btn class="btn-color submitBtn" @click="submit">Entrar</v-btn>
-      </router-link>
-    </form>
+      <div class="weather-image-wrapper">
+        <img src="../../assets/images/unlocked.svg" alt="Login" class="account-image">
+      </div>
+      <v-form class="wrapp-form" v-model="valid" ref="form" lazy-validation>
+        <v-text-field
+          label="E-mail"
+          v-model="email"
+          :rules="emailRules"
+          required
+        ></v-text-field>
+        <v-text-field
+          label="Senha"
+          v-model="pass"
+          :rules="passRules"
+          required
+        ></v-text-field>
+        
+        <div class="centered">
+          <v-btn
+            class="btn-color submitBtn"
+            @click="submit"
+            :disabled="!valid"
+          >
+            Entrar
+          </v-btn>
+        </div>
+      </v-form>
+    <!-- </v-app> -->
   </div>
   </div>
 </template>
@@ -38,36 +41,39 @@ export default {
 
   data: () => ({
     valid: true,
-    pass: '',
     name: '',
     nameRules: [
-      v => !!v || 'Nome é obrigatório',
-      v => (v && v.length <= 10) || 'Nome deve ter menos de 10 caracteres'
+      (v) => !!v || 'Nome é obrigatório',
+      (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
+    ],
+    phone: '',
+    phoneRules: [
+      (v) => !!v || 'Telefone é obrigatório'
+    ],
+    pass: '',
+    passRules: [
+      (v) => !!v || 'Senha é obrigatório',
+      (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
     ],
     email: '',
     emailRules: [
-      v => !!v || 'E-mail é obrigatório',
-      v =>
-        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(v) ||
-        'E-mail deve ser válido'
-    ],
-    phone: '',
-    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
-    checkbox: null
+      (v) => !!v || 'E-mail é obrigatório',
+      (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(v) || 'E-mail deve ser válido'
+    ]
   }),
 
   methods: {
     submit () {
-      if (this.$refs.form.validate()) {
+      // if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
         // axios.post('/api/submit', {
         //   name: this.name,
         //   email: this.email,
-        //   phone: this.phone,
+        //   select: this.select,
         //   checkbox: this.checkbox
-        // });
-      }
+        // })
+      // }
     },
-
     clear () {
       this.$refs.form.reset()
     }
@@ -78,28 +84,49 @@ export default {
 .wrapp-form
   width 90%
   margin 30px auto
+  text-align left 
+
+.centered 
+  margin 80px auto
+  padding-bottom 20px
+  text-align center
 
 .wrapp 
   position relative
-  height 100vh
-  padding 80px 25px;
+  opacity 0.9
+  background #fff
+  padding-top 30px
   font-family 'Dosis', sans-serif
 
 .submitBtn
   color #fff
-  margin-top 80px
+  text-align inherit 
   padding-left: 2.75em
   padding-right: 2.75em
   transition: background-color .3s ease
-  background-color #36d1b2
+  background-color #36d1b2 !important
   border 2px solid #36d1b2
   height 50px
   width: 80%;
   max-width 300px
-  border-radius: 2px;
-  bottom 15px
+  border-radius 2px
   font-size 1.25rem
-  font-family: 'Dosis', sans-serif;
+  font-family 'Dosis', sans-serif
+
+.clear 
+  padding-left 2.75em
+  padding-right 2.75em
+  height 50px
+  width 80%
+  max-width 300px
+  border-radius 2px
+  font-size 1.25rem
+  background #d6d6d6
+
+.submitBtn,
+.clear 
+  display inline-block
+  vertical-align middle
 
 .submitBtn:active, .submitBtn:hover
   background-color #60bfac
@@ -109,39 +136,33 @@ export default {
   width: 100px
 
 .title
-  font-size 40px !important
-  color #fff
+  font-size 50px !important
+  color #36d1b2
   margin: 5px 0 59px
 
 .hello
-  position: relative
-  min-height: 650px
-  background: url('../../../static/img/bg-hello-page.jpg')
-  background-position: center;
-  background-size: cover
+  position relative
+  min-height 100vh
+  padding 20px;
+  min-height 650px
+  background url('../../../static/img/bg-hello-page.jpg')
+  background-position center
+  background-size cover
 
 .hello::before
-  content: ''
-  position: absolute
-  width: 100%
-  height: 100%
-  top: 0
-  left: 0
-  opacity: 0.6;
-  background-image: linear-gradient(to right, #fa709a 0%, #fee140 100%)
+  content ''
+  position absolute
+  width 100%
+  height 100%
+  top 0
+  left 0
+  opacity 0.6
+  background-image linear-gradient(to right, #fa709a 0%, #fee140 100%)
 
-
-.color, .size
+.color
+  margin-top 60px
+  font-size 20px
   caret-color #fff !important
   color #fff !important
   font-size 20px !important
-
-.input-group--text-field label
-  font-size 22px !important
-
-.input-group.input-group--error .input-group__messages
- font-size 16px
-
-.input-group.input-group--error .input-group__details:before
-  opacity 1
 </style>
