5a22e6cf1b69f04c58ce3d7eb6b8db63b605e817
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-11-12 03:52:20 -0200

    Adicionando uikit, removendo bulma

--- client/src/main.js
+++ client/src/main.js
@@ -5,16 +5,19 @@ import App from './App'
 import router from './router'
 import Notifications from 'vue-notification'
 import Vulma from 'vulma'
-import Bulma from 'Bulma'
 import vueBulmaComponents from 'vue-bulma-components'
 import Vuetify from 'vuetify'
+
+import 'uikit/dist/css/uikit.min.css'
+import 'uikit/dist/js/uikit.min.js'
+import 'uikit/dist/js/uikit-icons'
+
 import '../node_modules/vuetify/dist/vuetify.min.css'
 import './assets/css/form.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Vulma)
-Vue.use(Bulma)
 Vue.use(Notifications)
 Vue.use(vueBulmaComponents)
 Vue.use(Vuetify)
