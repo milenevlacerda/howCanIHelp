6a717f22ec96c26699b6c936b8883871867ba376
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-11-12 03:48:34 -0200

    Criando componente de dados gerais

---
+++ client/src/components/generalData/GeneralData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <menu-component :title="title"></menu-component>
+    <tabs-component></tabs-component> 
+  </div>
+</template>
+<script>
+import menuComponent from '../menu/Menu'
+import tabsComponent from '../tabs/Tabs'
+
+export default {
+  name: 'GeneralData',
+
+  components: {
+    'menu-component': menuComponent,
+    'tabs-component': tabsComponent
+  },
+
+  data () {
+    return {
+      title: 'Pagamento'
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
+
