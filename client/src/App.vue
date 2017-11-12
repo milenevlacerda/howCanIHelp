cdabd2d0714722c082f821f6aa6eb954fd97fb1e
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-11-12 03:47:06 -0200

    Adicionando novos estilos padrão

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,38 +11,37 @@ export default {
 }
 </script>
 
-<style scoped>
-body {
-  margin: 0;
-}
+<style lang="stylus">
+body
+  margin 0
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  font-weight 400
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  color #2c3e50
 
-main {
-  text-align: center;
-}
+a
+  text-decoration none !important
 
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #00d1b2;
-  color: #fff;
-}
+main
+  text-align center
 
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+header
+  margin 0
+  height 56px
+  padding 0 16px 0 24px
+  background-color #00d1b2
+  color #fff
+
+header span
+  display block
+  position relative
+  font-size 20px
+  line-height 1
+  letter-spacing .02em
+  font-weight 400
+  box-sizing border-box
+  padding-top 16px
 </style>
