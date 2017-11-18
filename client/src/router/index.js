a1e5b7ebade38241130f4c5f21983cde5176537b
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-11-18 20:53:06 -0200

    Adicionando rotas de configurações de conta, projeto e perfil

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,6 +5,8 @@ import SignUp from '@/components/signUp/SignUp'
 import SignIn from '@/components/signIn/SignIn'
 import Timeline from '@/components/timeline/Timeline'
 import GeneralData from '@/components/generalData/GeneralData'
+import Project from '@/components/project/Project'
+import Profile from '@/components/profile/Profile'
 
 Vue.use(Router)
 
@@ -34,6 +36,16 @@ export default new Router({
       path: '/general',
       name: 'GeneralData',
       component: GeneralData
+    },
+    {
+      path: '/project',
+      name: 'Project',
+      component: Project
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
     }
   ]
 })
