0ecb27a8f541fbc3abd098ea3659893540782c20
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-12-03 03:42:00 -0200

    Adicionando formulário de cadastro de investimentos

---
+++ client/src/components/investments/Investments.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <menu-component :title="title"></menu-component>
+
+    <div class="wrapper">
+      <form @submit.prevent="save()">
+        <fieldset class="uk-fieldset">
+            <div class="uk-margin">
+                <input class="uk-input project-title" type="text" placeholder="Título">
+            </div>
+
+            <div class="uk-margin">
+                <input class="uk-input investment-value" type="number" placeholder="R$ Valor">
+            </div>
+
+            <div class="uk-margin">
+                <textarea class="uk-textarea" rows="5" placeholder="Descrição do investimento"></textarea>
+            </div>
+
+            <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+                <label class="checks"><input class="uk-checkbox" type="checkbox" checked>Investimento fixo</label>
+                <label class="checks"><input class="uk-checkbox" type="checkbox">Investimento não fixo</label>
+            </div>
+        </fieldset>
+
+        <div class="uk-button-group">
+          <button class="uk-button uk-button-primary" type="submit">Cadastrar</button>
+
+          <router-link :to="{ name: 'Timeline'}">
+            <button class="uk-button uk-button-default">Voltar</button>
+          </router-link>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import menuComponent from '../menu/Menu'
+
+export default {
+  name: 'Investments',
+
+  components: {
+    'menu-component': menuComponent
+  },
+
+  data () {
+    return {
+      title: 'Investimentos'
+    }
+  },
+
+  methods: {
+    save () {
+      console.log('oi')
+      // this.service.cadastra(this.foto)
+      // .then( ()=> this.foto = new Foto, err => console.log(err) );
+    }
+  },
+
+  created () {
+    console.log('oi')
+    // this.service = new FotoService( this.$resource );
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper 
+  max-width 1200px
+  padding 90px 20px 20px
+  margin 0 auto
+
+.project-title
+  font-size 25px
+  color #a2a2a2
+  font-weight 800
+
+.checks input
+  margin-right 10px
+  display inline-block
+  vertical-align middle
+
+.file
+  text-align left 
+
+.uk-button-group
+  width 100%
+  text-align left 
+
+  button
+    margin-right 20px
+    font-weight 800
+    letter-spacing 1px
+    font-size 13px
+
+.uk-button-primary
+  background-color #4ed2b2
+
+textarea
+  resize none
+
+.investment-value
+  width 100%
+
+</style>
