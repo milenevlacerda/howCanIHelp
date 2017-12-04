e0c19ae50624f0cc58201d74da84688740748dcc
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-12-03 22:56:43 -0200

    Criando tela de histórico de doações

---
+++ client/src/components/donationHistory/DonationHistory.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <menu-component :title="title"></menu-component>
+    
+    <div class="wrapper">
+      <line-chart></line-chart>
+    </div>
+
+  </div>
+</template>
+<script>
+import menuComponent from '../menu/Menu'
+import lineChart from '../line-chart/LineChart'
+
+export default {
+  name: 'DonationHistory',
+
+  components: {
+    'menu-component': menuComponent,
+    'line-chart': lineChart
+  },
+
+  data () {
+    return {
+      title: 'Histórico de Doações'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+  margin 0 auto
+  padding 90px 20px 20px
+
+.wrapper-card
+  max-width 1000px
+  padding-top 20px
+  margin 0 auto 10px
+
+</style>
+
