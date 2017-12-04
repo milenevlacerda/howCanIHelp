53f200c34e9af4b18557a0ae721e41c5b0426f3d
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-12-03 22:52:24 -0200

    Adicionando modal de about de pessoa/ong/projeto no perfil

--- client/src/components/profile/Profile.vue
+++ client/src/components/profile/Profile.vue
@@ -17,7 +17,6 @@
 
       <!-- This is an anchor toggling the modal -->
       <a href="#modal-example" uk-toggle class="about">SOBRE</a>
-
       <!-- This is the modal -->
       <div id="modal-example" uk-modal>
         <div class="uk-modal-dialog uk-modal-body">
@@ -29,12 +28,25 @@
         </div>
       </div>
 
+      <div id="modal-exa" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+          <h2 class="uk-modal-title">Título do projeto</h2>
+          <p>Sobre o projeto, dados preenchidos no cadastro do projeto</p>
+          <p class="uk-text-right">
+            <router-link :to="{ name: 'GeneralData'}">
+              <button class="uk-button uk-button-primary uk-modal-close" type="button">Doar</button>
+            </router-link>
+            <button class="uk-button uk-button-default uk-modal-close" type="button">Fechar</button>
+          </p>
+        </div>
+      </div>
+
       <div class="slider">
-        <a href="" >
+        <a href="#modal-exa" uk-toggle class="hover">
           <h3 class="project-name uk-text-uppercase uk-text-small">Projeto 1</h3>
+          <h6 class="more-about"> Ver Mais</h6>
           <img src="../../../static/img/project0.jpg" alt="">
         </a>
-
         <!-- <div href="">
           <h3 class="project-name uk-text-uppercase uk-text-small">Projeto 1</h3>
           <img src="../../../static/img/project0.jpg" alt="">
@@ -149,15 +161,41 @@ export default {
   font-size 25px
   color #fff
 
+.hover:hover
+  .more-about
+    opacity 1
+
+  &::before 
+    background rgba(105, 105, 105, 0.8)
+
+.uk-button-primary
+  background-color #4ed2b2
+
 .project-name
   position absolute
   padding 10px
+  color #fff
+
+.more-about
+  position absolute
+  font-size 32px
+  font-weight 200
+  left 50%
+  top 50%
+  transform translate3d(-50%, -50%, 0)
+  margin 0
+  opacity 0
+  transition .5s ease-in-out
   color #fff
 
 .link-modal
   position relative
   bottom 0
   z-index 1000
+
+.icon-plus
+  display inline-block
+  vertical-align middle
 
 .about
   font-weight 800
